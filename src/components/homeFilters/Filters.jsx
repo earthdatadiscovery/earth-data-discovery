13f5184113dbfbf5524d64c83dfdf9404fa77de0
nicolasvienot <nvienot@student.42.fr>
2020-10-04 07:04:06 +0200

    search working

--- src/components/homeFilters/Filters.jsx
+++ src/components/homeFilters/Filters.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-export default function Filters() {
+export default function Filters({ topPosition }) {
   return <WrapperFilters></WrapperFilters>;
 }
 
 const WrapperFilters = styled.div`
   position: absolute;
-  bottom: 0;
+  top: ${(props) => props.topPosition || 0}%;
   width: 100%;
   height: 40px;
   background-color: #3c3c3c;
